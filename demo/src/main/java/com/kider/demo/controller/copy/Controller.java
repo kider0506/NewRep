9c6cf3806813a6d5b1ab849744dc3a3fa4dd7142
Kider <kidernj16@gmail.com>
2022-03-09 06:55:28 +0530

    Junit Covered for Controller

--- demo/src/main/java/com/kider/demo/controller/copy/Controller.java
+++ demo/src/main/java/com/kider/demo/controller/copy/Controller.java
@@ -1,14 +1,19 @@
 package com.kider.demo.controller.copy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller{
 	
+@Value("${name}")
+public String name;
+	
 @RequestMapping("/v1")
 public String meth1(){
-	return "hi";
+	String i = name;
+	return i;
 }
 	
 
